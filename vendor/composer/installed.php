6cc596c2717feb3e8e81c43f9f22fe764d5c8f0f
Aurélien Pohu <aurelien.pohu@gmail.com>
2023-09-13 13:02:09 +0000

    update github workflows

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,32 +1,33 @@
-<?php return array(
-    'root' => array(
-        'name' => '__root__',
-        'pretty_version' => '3.4.5.x-dev',
-        'version' => '3.4.5.9999999-dev',
-        'reference' => '37e03020289b54edee86c7b2441de96be47dd119',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        '__root__' => array(
-            'pretty_version' => '3.4.5.x-dev',
-            'version' => '3.4.5.9999999-dev',
-            'reference' => '37e03020289b54edee86c7b2441de96be47dd119',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'james-heinrich/getid3' => array(
-            'pretty_version' => '2.0.x-dev',
-            'version' => '2.0.9999999.9999999-dev',
-            'reference' => '4e7aca96e7f4cf5ce6e08bca998334f567259965',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../james-heinrich/getid3',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
+    'reference' => '03030f03b422a6d3488c2adce026da0ab8d3ea30',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '03030f03b422a6d3488c2adce026da0ab8d3ea30',
     ),
+    'james-heinrich/getid3' => 
+    array (
+      'pretty_version' => '2.0.x-dev',
+      'version' => '2.0.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4e7aca96e7f4cf5ce6e08bca998334f567259965',
+    ),
+  ),
 );
